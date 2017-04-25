b212dad9b5e4a9c2aba140b483685f1b5f626c23
ardMac <adliramli88@gmail.com>
2017-04-25 10:50:36 +0800

    Initial Commit

---
+++ APiPokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APiPokedex
+//
+//  Created by ardMac on 25/04/2017.
+//  Copyright © 2017 ardMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
